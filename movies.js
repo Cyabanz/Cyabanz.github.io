1a39297e83f3cf3d58d8292f38f1579ca357f48e
Cyabanz <118572258+Cyabanz@users.noreply.github.com>
2025-05-26 09:42:21 -0700

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,10 @@
+/**
+ * these are the secrets (api keys) for stuff
+ * they're all from free services; no point in skidding!
+ */
+
+// mov
+const movSec = "2713804610e1e236b1cf44bfac3a7776";
+
+// ai
+const aiSec = "gsk0TD3dvCyLaoy2sHiAy4KWGdyb3FYz8bWmlXMPe6vIsgAaNhri64U";
